63342601b2751fcaba1da5ab3ea652f2df7ec32c
s-sameer <itsnotsameer@gmail.com>
2022-10-28 07:49:22 -0230

    imported image

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,13 @@
+import image from "../images/bg.jpg"
+
 function Body() {
   return (
-    <div className="grid grid-cols-2 h-fit mx-auto gap-5 p-5">
+    <div className="grid grid-cols-2 h-fit mx-auto gap-5 p-5 ">
         <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=640:*" height="300px" width="600px"/>
         <img src="https://images.hindustantimes.com/img/2022/03/12/550x309/parvovirus_thumb_1647089326234_1647089335606.jpg" style={{width: 600, height:300}}/>
         <img src="https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg" width={600} height={300} />
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSor_QkJKGs4txuGym5kYPfTSPkaulc-LFd59WiRvspeSrvv3ZGtSAFfPe9ZYqdhJlD8Yg&usqp=CAU" height="300px" width="600px"/>
+        <img src={image} />
     </div>
   )
 }
